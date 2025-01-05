1b6f8d135a52c7cb3d5bb819a03fd0afc9632f1d
mzaedan <zaedanm108@gmail.com>
2025-01-05 20:37:11 +0700

    update CRUD Admin Area

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,11 @@ use App\Http\Controllers\DashboardProductController;
 use App\Http\Controllers\DashboardSettingController;
 use App\Http\Controllers\DashboardTransactionController;
 use App\Http\Controllers\DetailController;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
+use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
+use App\Http\Controllers\Admin\UserController as AdminUserController;
+use App\Http\Controllers\Admin\ProductController as AdminProductController;
+use App\Http\Controllers\Admin\ProductGalleryController as AdminProductGalleryController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
@@ -35,5 +40,16 @@ Route::get('/dashboard/transactions/{id}', [DashboardTransactionController::clas
 
 Route::get('/dashboard/settings', [DashboardSettingController::class, 'store'])->name('dashboard-settings-store');
 Route::get('/dashboard/account', [DashboardSettingController::class, 'account'])->name('dashboard-settings-account');
+
+Route::prefix('admin')
+    ->namespace('App\Http\Controllers\Admin')
+    // ->middleware(['auth', 'admin'])
+    ->group(function () {
+        Route::get('/', [AdminDashboardController::class, 'index'])->name('admin-dashboard');
+        Route::resource('category', AdminCategoryController::class);
+        Route::resource('user', AdminUserController::class);
+        Route::resource('product', AdminProductController::class);
+        Route::resource('product-gallery', AdminProductGalleryController::class);
+    });
 
 
